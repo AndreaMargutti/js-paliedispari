1e5e96a849fe6e178b7ab6d59136c0443ed4c3a5
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-22 14:53:26 +0200

    insert prompt

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+//Chiedere la parola all'utente
+const userWord = prompt('Dammi una parola').trim();
+console.log(userWord);
