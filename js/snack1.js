bfd82bbf8c18ce8bc7897b1da3c65ed4a3dc9398
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-22 15:30:22 +0200

    better comments and file

--- js/snack1.js
+++ js/snack1.js
@@ -4,12 +4,14 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// # Fase di preparazione
 //Chiedere la parola all'utente
 const userWord = prompt('Dammi una parola').trim();
 console.log(userWord);
 //creo una variabile dove verrà inserita la parola al contrario
 let reverseWord = '';
 
+// # Fase di elaborazione dei dati
 //dichiaro la funzione
 function isPalindroma(word){
     let flag =false;
@@ -24,5 +26,6 @@ function isPalindroma(word){
     console.log(flag);
 }
 
+// # Fase di output
 //richiamo la funzione assegnandole il valore di userWord
 isPalindroma(userWord);
